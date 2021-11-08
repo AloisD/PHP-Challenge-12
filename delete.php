27f0440ff8a5adde11260bd898846e1d897d952d
Aloïs Delaune <90683963+AloisD@users.noreply.github.com>
2021-11-08 14:49:06 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,6 +5,13 @@ define("BR", "<br><br><br>");
 if (!is_dir($_GET["delete"])) {
     unlink($_GET["delete"]);
 } else {
+    $files = scandir($_GET["delete"]);
+    foreach ($files as $key => $value) {
+        $path = realpath($_GET["delete"] . DIRECTORY_SEPARATOR . $value);
+        if (!is_dir($path)) {
+            unlink($path);
+        }
+    }
     rmdir($_GET["delete"]);
 }
 
